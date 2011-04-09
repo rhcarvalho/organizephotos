6bc76f8c908556fde555c7997d5b764e481a17df
Rodolfo Carvalho <rhcarvalho@gmail.com>
2011-04-09 20:01:58 -0300

    Add command-line support (not fully functional yet).

--- match_photos.py
+++ match_photos.py
@@ -2,17 +2,42 @@
 # this should be part of http://launchpad.net/organizephotos/
 
 from collections import defaultdict
-from os.path import basename
+from os import listdir
+from os.path import basename, join
+import sys
 
 
-def match_photos(low_resolution_paths, high_resolution_paths):
-    low_resolution_names = (basename(path) for path in low_resolution_paths)
-    high_resolution_matchs = defaultdict(list)
-    for path in high_resolution_paths:
-        high_resolution_matchs[basename(path)].append(path)
+def match_photos(source_paths, destination_paths):
+    '''Find files in `destination_paths` with the same name as of the files
+    in `source_paths`.'''
+    source_names = (basename(path) for path in source_paths)
+    destination_matchs = defaultdict(list)
+    for path in destination_paths:
+        destination_matchs[basename(path)].append(path)
+    return (destination_matchs[name] for name in source_names)
     
-    return [high_resolution_matchs[name] for name in low_resolution_names]
-
+def main():
+    '''Basic implementation of a main function.
+    
+    Currently it simply lists the directories FROM and TO, not checking file
+    types and not recursing the tree.'''
+    
+    # Check if the script was called with the right number of arguments
+    if len(sys.argv) != 3:
+        print >>sys.stderr, "Usage: %s FROM TO" % sys.argv[0]
+        sys.exit(1)
+    
+    # List the directories. This part should be improved to recurse the tree
+    # and maybe filter only .jpg files.
+    src, dest = map(listdir, sys.argv[1:])
+    
+    for match in match_photos(src, dest):
+        # Ignore empty matches. Maybe we should tell the user that there were
+        # no correspondence found for some files.
+        if match:
+            # Since os.listdir remove path information, it is joined back before
+            # printing.
+            print ', '.join(join(sys.argv[2], path) for path in match)
 
 #-------------------------------------------------------------------------------
 
@@ -93,4 +118,5 @@ class TestMatchPhotos(unittest.TestCase):
         self.assertEqual(match_photos(low_res, hi_res), match)
 
 if __name__ == '__main__':
-    unittest.main()
+#    unittest.main()
+    main()
